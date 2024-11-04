eeaaef0e8e680ede61b0bc4a01f2d600629d0f1f
tapiwanasheMbizvo <tapiwambizvo@gmail.com>
2024-11-04 02:35:14 +0200

    working with read replica

--- 00_init.sql
+++ 00_init.sql
@@ -1,2 +1,4 @@
 create user replicator with replication encrypted password 'replicator_password';
 select pg_create_physical_replication_slot('replication_slot');
+
+--create user ripple with replication encrypted password 'ripple_password';
